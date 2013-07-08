109f54e853547345520ea46118809d26b5442914
Farhan <farhan-HP-ProBook-4430s>
2013-07-09 01:47:21 +0600

    key listing

--- Idkey-new/src/com/prologic/idkey/activities/KeyListingActivities.java
+++ Idkey-new/src/com/prologic/idkey/activities/KeyListingActivities.java
@@ -5,11 +5,16 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import com.prologic.idkey.CustomProgressDailog;
 import com.prologic.idkey.R;
+import com.prologic.idkey.api.ApiConnection;
+import com.prologic.idkey.api.command.GetKeysCommand;
 import com.prologic.idkey.objects.Key;
 import com.prologic.idkey.objects.KeyListAdapter;
 import com.prologic.idkey.objects.KeysComparator;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -30,7 +35,8 @@ public class KeyListingActivities extends MainActivity implements OnClickListene
 	private Button btnKeySortId;
 	private Button btnKeySortCat;
 	private KeysComparator keysComparator;
-    private EditText etSearch;
+	private EditText etSearch;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -40,7 +46,7 @@ public class KeyListingActivities extends MainActivity implements OnClickListene
 		btnNoKeySort = (Button) findViewById(R.id.btn_key_sort_id);
 		btnKeySortId = (Button) findViewById(R.id.btn_key_sort_name);
 		btnKeySortCat = (Button) findViewById(R.id.btn_key_sort_cat);
-        etSearch = (EditText) findViewById(R.id.et_key_search);
+		etSearch = (EditText) findViewById(R.id.et_key_search);
 		listKeys = new ArrayList<Key>();
 		adapter = new KeyListAdapter(this, R.layout.key_list_row_view, listKeys);
 		lvKeys.setAdapter(adapter);
@@ -52,27 +58,27 @@ public class KeyListingActivities extends MainActivity implements OnClickListene
 		keysComparator = new KeysComparator(KeysComparator.SORTING_TYPE_ID, KeysComparator.SORTING_ORDER_ASCENDING);
 
 
-        etSearch.addTextChangedListener(new TextWatcher() {
+		etSearch.addTextChangedListener(new TextWatcher() {
 
-            @Override
-            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-                // When user changed the Text
-                adapter.getFilter().filter(cs);
-            }
+			@Override
+			public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+				// When user changed the Text
+				adapter.getFilter().filter(cs);
+			}
 
-            @Override
-            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-                                          int arg3) {
-                // TODO Auto-generated method stub
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
 
-            }
+			}
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+			@Override
+			public void afterTextChanged(Editable editable) {
 
-            }
-        });
-        loadKeys();
+			}
+		});
+		loadKeys();
 
 
 
@@ -80,7 +86,7 @@ public class KeyListingActivities extends MainActivity implements OnClickListene
 
 	private void loadKeys()
 	{
-		Key key = new Key(1, "aoom 15", "11/12/13", "Raddison");
+		/*Key key = new Key(1, "aoom 15", "11/12/13", "Raddison");
 		Key key1 = new Key(2, "room 19", "11/12/13", "Raddison blu");
 		Key key2 = new Key(3, "zoom 16","11/12/13", "sonargaon");
 		Key key3 = new Key(4, "poom 17","11/12/13", "Hilton");
@@ -92,8 +98,20 @@ public class KeyListingActivities extends MainActivity implements OnClickListene
 		listKeys.add(key1);
 		listKeys.add(key3);
 
-		adapter.notifyDataSetChanged();
+		adapter.notifyDataSetChanged();*/
+		new KeyLoadTask(context).execute();
 
+
+	}
+
+	private void updateKeyList(List<Key> keys)
+	{
+		if(keys != null)
+		{
+			listKeys.clear();
+			listKeys.addAll(keys);
+			adapter.notifyDataSetChanged();
+		}	
 
 	}
 	@Override
@@ -121,6 +139,54 @@ public class KeyListingActivities extends MainActivity implements OnClickListene
 		default:
 			break;
 		}
+	}
+
+	private class KeyLoadTask extends AsyncTask<Void, Void, Void>
+	{
+		private int userCategoryId;
+		private Context context;
+		private CustomProgressDailog progressDialog;
+		private GetKeysCommand keysCommand;
+
+		public KeyLoadTask(Context context) 
+		{
+			this(context,-1);			
+		}
+		public KeyLoadTask(Context context,int userCategoryId) 
+		{
+			this.context = context;
+			this.userCategoryId = userCategoryId;
+			progressDialog = new CustomProgressDailog(context);
+			progressDialog.setTitle("Loading");
+			progressDialog.setMessage("Please wait...");
+
+		}
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progressDialog.show();
+		}
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+
+			if(progressDialog.isShowing())
+			{
+				progressDialog.dismiss();
+			}
+			updateKeyList(keysCommand.getListKeys());
+			keysCommand = null;		
+
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) 
+		{
+			keysCommand = new GetKeysCommand(userCategoryId);
+			keysCommand.execute(ApiConnection.getInstance(context));
+			return null;
+		}
+
 	}
 
 }
--- Idkey-new/src/com/prologic/idkey/api/command/GetKeysCommand.java
+++ Idkey-new/src/com/prologic/idkey/api/command/GetKeysCommand.java
@@ -1,5 +1,9 @@
 package com.prologic.idkey.api.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,23 +12,31 @@ import android.util.Log;
 import com.prologic.idkey.api.ApiConnection;
 import com.prologic.idkey.api.ApiException;
 import com.prologic.idkey.api.JsonCommand;
+import com.prologic.idkey.objects.Category;
+import com.prologic.idkey.objects.Key;
 
 public class GetKeysCommand extends JsonCommand {
 
 	int userCategoryId;
+	private List<Key> listKeys;
 	public GetKeysCommand()
 	{
 		userCategoryId = -1;
-		
+
 	}
 	public GetKeysCommand(int userCategoryId) 
 	{
 		this.userCategoryId = userCategoryId;
 	}
+	public List<Key> getListKeys() {
+		return listKeys;
+	}
+	
+
 	@Override
 	protected String makeRequest(ApiConnection apiConnection)
 			throws ApiException {
-	//http://50.57.84.233/cms/api/get_keys.json?auth_token=xM2e7dJNx5f5Ey7pxQqC
+		//http://50.57.84.233/cms/api/get_keys.json?auth_token=xM2e7dJNx5f5Ey7pxQqC
 		String url = "http://50.57.84.233/cms/api/get_keys.json?auth_token=xM2e7dJNx5f5Ey7pxQqC"; //ApiConnection.mainUrl+"get_keys.json"+"?auth_token="+Session.getInstance().getAuthToken();		
 		JSONObject jsonObj = null;
 		if(userCategoryId >-1)
@@ -32,19 +44,92 @@ public class GetKeysCommand extends JsonCommand {
 			try {
 				jsonObj = new JSONObject();
 				jsonObj.put("user_category_id", userCategoryId);
-			
+
 			} catch (JSONException e) 
 			{			
 				Log.e(TAG, e.getMessage());
 			}
 		}
-		return apiConnection.get(url,jsonObj.toString());
+		String body = (jsonObj == null)? null : jsonObj.toString();
+		
+		return apiConnection.get(url,body);
 	}
 
 	@Override
-	protected void parse(String data) throws JSONException {
-		
-		
+	protected void parse(String data) throws JSONException 
+	{
+		try {
+			JSONObject jsonObject = new JSONObject(data);
+
+			if(jsonObject.has("user_keys") && jsonObject.get("user_keys") instanceof JSONArray)
+			{
+				JSONArray userKeysJsonArray = jsonObject.getJSONArray("user_keys");
+
+				if(userKeysJsonArray.length() >0)
+				{
+					listKeys = new ArrayList<Key>();
+					for(int i = 0; i<userKeysJsonArray.length(); i++)
+					{
+						if(userKeysJsonArray.get(i) instanceof JSONObject)
+						{
+							int id = -1;
+							int category_id = -1;
+							String name = "";							
+							String iqe_token = "";
+							String scan_url = "";
+							String category_name = "";
+							String created = "";
+
+							JSONObject jsonCategoryObj = userKeysJsonArray.getJSONObject(i);
+							if(jsonCategoryObj.has("id"))
+							{
+								id = jsonCategoryObj.getInt("id");								
+							}
+							if(jsonCategoryObj.has("category_id"))
+							{
+								category_id = jsonCategoryObj.getInt("category_id");								
+							}
+							if(jsonCategoryObj.has("name"))
+							{
+								name = jsonCategoryObj.getString("name");								
+							}
+							if(jsonCategoryObj.has("iqe_token"))
+							{
+								iqe_token = jsonCategoryObj.getString("iqe_token");								
+							}
+							if(jsonCategoryObj.has("scan_url"))
+							{
+								scan_url = jsonCategoryObj.getString("scan_url");								
+							}
+							if(jsonCategoryObj.has("category_name"))
+							{
+								category_name = jsonCategoryObj.getString("category_name");								
+							}
+							if(jsonCategoryObj.has("created"))
+							{
+								created = jsonCategoryObj.getString("created");								
+							}
+							if(id >-1)
+							{
+								Key key = new Key(id, name, created, category_name);
+								key.setIqeToken(iqe_token);
+								key.setCategoryId(category_id);
+								key.setScanUrl(scan_url);
+								
+								listKeys.add(key);
+							}
+						}
+					}
+
+				}
+
+			}
+		}
+		catch (JSONException e) {
+			Log.e(TAG, e.getMessage());
+		}
+
+
 	}
 
 }
